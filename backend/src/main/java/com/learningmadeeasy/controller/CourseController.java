afe5756d4a3247082189b568d4e7c8b662081b57
ravii-1996 <30302049+ravii-1996@users.noreply.github.com>
2021-03-01 17:14:16 +0530

    Update CourseController.java
    API End Point updated.

--- backend/src/main/java/com/learningmadeeasy/controller/CourseController.java
+++ backend/src/main/java/com/learningmadeeasy/controller/CourseController.java
@@ -1,7 +1,6 @@
 package com.learningmadeeasy.controller;
 
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
